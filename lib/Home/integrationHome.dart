3cf3736a89d3abf799a649514c9fa7c251faadf1
nahyunuk <s24022@gsm.hs.kr>
2024-10-03 15:12:53 +0900

    add : page

---
+++ lib/Home/integrationHome.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:pratice_2023_3/Home/bottunNavigationBar.dart';
+import 'package:pratice_2023_3/Home/mainHome.dart';
+class IntegrationHome extends StatefulWidget {
+  const IntegrationHome({super.key});
+
+  @override
+  State<IntegrationHome> createState() => _IntegrationHomeState();
+}
+
+class _IntegrationHomeState extends State<IntegrationHome> {
+  int _currentIndex = 0; // 현재 선택된 탭의 인덱스
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _getSelectedPage(_currentIndex), // 현재 선택된 페이지에 맞는 위젯 표시
+      ),
+      bottomNavigationBar: BottunnavigationBar(
+        currentIndex: _currentIndex,
+        onTabSelected: _onTabSelected,
+      ), // 분리된 파일에서 BottomNavigationBar 사용
+    );
+  }
+
+  Widget _getSelectedPage(int index) {
+    switch (index) {
+      case 0:
+        return Mainhome();
+      case 1:
+        return Text('Search Page');
+      case 2:
+        return Text('Profile Page');
+      default:
+        return Text('Unknown Page');
+    }
+  }
+}
