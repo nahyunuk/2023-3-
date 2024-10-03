dcd93264fd59c1eee326957ae11b556863ced1c6
nahyunuk <s24022@gsm.hs.kr>
2024-10-03 15:12:20 +0900

    add : page

---
+++ lib/Home/bakyungage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Bakyungage extends StatefulWidget {
+  const Bakyungage({super.key});
+
+  @override
+  State<Bakyungage> createState() => _BakyungageState();
+}
+
+class _BakyungageState extends State<Bakyungage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
